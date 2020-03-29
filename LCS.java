5e4fbdf8f7bead5b13f5a44e65950e42e8bd54d1
yamadadaiki <daiki.baseball0516@gmail.com>
2020-03-29 18:16:36 +0900

    0329commit

--- LCS.java
+++ LCS.java
@@ -4,36 +4,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         String t = sc.next();
-        int DP [][] = new int[s.length()+1][t.length()+1];
-        int List [][] = new int[s.length()+1][t.length()+1];
-        StringBuilder opt = new StringBuilder();
-        for(int i=0;i<s.length()+1;i++) DP[i][0]=0;
-        for(int j=0;j<t.length()+1;j++) DP[0][j]=0;
-        for(int i=1;i<=s.length();i++){
-            for(int j=1;j<=t.length();j++){
+        int DP[][] = new int[s.length()+1][t.length()+1];
+        int L [][] = new int[s.length()+1][t.length()+1];
+        for(int i=0;i<s.length()+1;i++) {
+            Arrays.fill(DP[i],0);
+            Arrays.fill(L[i],0);
+        }
+        for(int i=1;i<s.length()+1;i++){
+            for(int j=1;j<t.length()+1;j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    DP[i][j]=DP[i-1][j-1]+1;
-                    List[i][j] = 2;
+                    DP[i][j] = DP[i-1][j-1]+1;
+                    L[i][j] = 2;
                 }
-                else if(s.charAt(i-1)!=t.charAt(j-1)) {
-                    DP[i][j] = Math.max(DP[i][j - 1], DP[i - 1][j]);
-                    if(DP[i][j]==DP[i][j-1])List[i][j]=3;
-                    else List[i][j]=1;
+                else {
+                    DP[i][j] = Math.max(DP[i-1][j],DP[i][j-1]);
+                    if(DP[i][j]==DP[i-1][j])L[i][j] =1;
+                    else L[i][j]=3;
                 }
             }
         }
-       int i = s.length(), j = t.length();
+        StringBuilder opt = new StringBuilder();
+        int i = s.length(), j = t.length();
         for(int k=0;k<s.length()*t.length();k++){
-            if(s.charAt(i-1)==t.charAt(j-1)) opt.append(s.charAt(i-1));
-            if(List[i][j]==1) i--;
-            else if(List[i][j]==3) j--;
-            else if(List[i][j]==2){
+            if(s.charAt(i-1)==t.charAt(j-1)) opt.insert(0,s.charAt(i-1));
+            if(L[i][j]==1) i--;
+            else if(L[i][j]==3) j--;
+            else if(L[i][j]==2){
                 i--;
                 j--;
             }
             if(i==0||j==0) break;
         }
-        for(i=opt.length()-1;i>=0;i--)System.out.print(opt.charAt(i));
-        System.out.println();
+        System.out.println(opt);
     }
 }
